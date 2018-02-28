6dd36965d0519b3f1605c3da65b8d9baa9e2a0ab
Zachary Bloss <zacharybloss@gmail.com>
2018-02-28 13:15:09 -0500

    Completed Linear Regression on scikitlearns load_boston data

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# us-mortgage-ml
-Training a Linear Regression model to predict Boston housing prices based off of a set of features. 
+### us-mortgage-ml ###
+Training a Linear Regression model to predict Boston housing prices based off of a set of features.
+
+Doing exploratory Data Analysis and Data Visualization on real-world housing data from Boston, MA.
+
+### Steps ###
+
+All work has been done in main.py.
+1. import data from scikitlearn
+2. format data into a DataFrame
+3. perform various visualizations to gain insight
+4. train a linear regression model and make Predictions
+5. compare the predictions against the actual data with a scatter plot
+
+### Visualizations ###
+All Visualizations have been exported to the /plots/ directory
+1. ActualVPredMEDV.png
+  - Here is the scatterplot comparing the final results of the data
+2. BostonHousingCorrHeatmap.png
+  - A heatmap showing the correlation each feature has with the Median House value
+3. BostonHousingDistPlot
+  - a distribution plot using Seaborn of the Median House Value
+
+### DATASET DESCRIPTION ###
+Boston House Prices dataset
+===========================
+
+Notes
+------
+Data Set Characteristics:  
+
+    :Number of Instances: 506
+
+    :Number of Attributes: 13 numeric/categorical predictive
+
+    :Median Value (attribute 14) is usually the target
+
+    :Attribute Information (in order):
+        - CRIM     per capita crime rate by town
+        - ZN       proportion of residential land zoned for lots over 25,000 sq.ft.
+        - INDUS    proportion of non-retail business acres per town
+        - CHAS     Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)
+        - NOX      nitric oxides concentration (parts per 10 million)
+        - RM       average number of rooms per dwelling
+        - AGE      proportion of owner-occupied units built prior to 1940
+        - DIS      weighted distances to five Boston employment centres
+        - RAD      index of accessibility to radial highways
+        - TAX      full-value property-tax rate per $10,000
+        - PTRATIO  pupil-teacher ratio by town
+        - B        1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town
+        - LSTAT    % lower status of the population
+        - MEDV     Median value of owner-occupied homes in $1000's
+
+    :Missing Attribute Values: None
+
+    :Creator: Harrison, D. and Rubinfeld, D.L.
+
+This is a copy of UCI ML housing dataset.
+http://archive.ics.uci.edu/ml/datasets/Housing
+
+
+This dataset was taken from the StatLib library which is maintained at Carnegie Mellon University.
+
+The Boston house-price data of Harrison, D. and Rubinfeld, D.L. 'Hedonic
+prices and the demand for clean air', J. Environ. Economics & Management,
+vol.5, 81-102, 1978.   Used in Belsley, Kuh & Welsch, 'Regression diagnostics
+...', Wiley, 1980.   N.B. Various transformations are used in the table on
+pages 244-261 of the latter.
+
+The Boston house-price data has been used in many machine learning papers that address regression
+problems.   
+
+**References**
+
+   - Belsley, Kuh & Welsch, 'Regression diagnostics: Identifying Influential Data and Sources of Collinearity', Wiley, 1980. 244-261.
+   - Quinlan,R. (1993). Combining Instance-Based and Model-Based Learning. In Proceedings on the Tenth International Conference of Machine Learning, 236-243, University of Massachusetts, Amherst. Morgan Kaufmann.
+   - many more! (see http://archive.ics.uci.edu/ml/datasets/Housing)
+
+  ###  ###
---
+++ main.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_boston
+from sklearn.metrics import classification_report, mean_squared_error
+from pylab import savefig
+
+# Setting the project path. This will be used to save various plots
+
+
+# Using real-world data from scikitlearns load_boston dataset
+boston = load_boston()
+print(boston['DESCR'])
+boston.keys()
+# Creating a Pandas DataFrame from the boston data and feature_names in order
+# to more easily maniupalte the data.
+boston_df = pd.DataFrame(boston['data'], columns=boston['feature_names'])
+target_df = pd.DataFrame(boston['target'], columns=['MEDV'])
+boston_df = pd.concat([boston_df, target_df], axis=1)
+
+# Checking for NaN values
+boston_df.isnull().values.any()
+
+boston_df.info()
+boston_df.describe()
+
+'''
+Boston House Prices dataset
+===========================
+
+Notes
+------
+Data Set Characteristics:
+
+    :Number of Instances: 506
+
+    :Number of Attributes: 13 numeric/categorical predictive
+
+    :Median Value (attribute 14) is usually the target
+
+    :Attribute Information (in order):
+        - CRIM     per capita crime rate by town
+        - ZN       proportion of residential land zoned for lots over 25,000 sq.ft.
+        - INDUS    proportion of non-retail business acres per town
+        - CHAS     Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)
+        - NOX      nitric oxides concentration (parts per 10 million)
+        - RM       average number of rooms per dwelling
+        - AGE      proportion of owner-occupied units built prior to 1940
+        - DIS      weighted distances to five Boston employment centrescentres
+        - RAD      index of accessibility to radial highways
+        - TAX      full-value property-tax rate per $10,000
+        - PTRATIO  pupil-teacher ratio by town
+        - B        1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town
+        - LSTAT    % lower status of the population
+        - MEDV     Median value of owner-occupied homes in $1000's
+'''
+plots_path = '/home/zbloss/Github/us-mortgage-ml/plots/'
+data_path = '/home/zbloss/Github/us-mortgage-ml/data/'
+
+
+boston_distplot = sns.distplot(boston_df['MEDV'])
+boston_distplot.get_figure().savefig(plots_path + "BostonHousingDistPlot.jpg")
+
+sns.distplot(boston_df['MEDV'])
+
+bdf_corr = boston_df.corr()
+
+# Here we see which features have the strongest correlation on Median value
+bdf_corr['MEDV'].sort_values(ascending=False)
+# We can see the factor with the largest correlation is number of rooms
+
+plt.figure(figsize=(10,10))
+sns.heatmap(boston_df.corr(), annot=True, cmap='coolwarm')
+
+plt.figure(figsize=(10,10))
+sns.heatmap(boston_df.corr(), annot=True, cmap='coolwarm').get_figure().savefig(plots_path + 'BostonHousingCorrHeatmap.png')
+
+# Exporting to excel spreadsheet
+boston_df.to_excel(data_path + 'boston_df.xls')
+
+# Now the train and test variables are separated
+X = boston_df.drop('MEDV', axis=1)
+y = boston_df['MEDV']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+# Model gets instantiated and trained on the training data
+lrm = LinearRegression()
+lrm.fit(X_train, y_train)
+
+# predictions are made and compared to the test data
+preds = lrm.predict(X_test)
+
+
+print(lrm.intercept_)
+print(lrm.coef_)
+
+plt.figure(figsize=(16,8))
+plt.scatter(y_test, preds)
+plt.xlabel('Actual Housing Data')
+plt.ylabel('Predicted Housing Data')
+plt.title('Actual vs Predicted Median Housing Values')
+savefig(plots_path + 'ActualVPredMEDV.png')
+
+
+### Calculating MSE ###
+
+MSE = mean_squared_error(y_test, preds)
+print(MSE)
